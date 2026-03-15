54cb824374dd58e249ef86e44aa3dfb431df8613
test 01 - Nguyen Ba Bao Tram <baotram11496@gmail.com>
2026-03-16 01:48:25 +0700

    feat: add solution for challenge 05

---
+++ tests/lesson-04/final-stage.js
@@ -0,0 +1,16 @@
+//Create function findPairDivisibleBy17
+function findPairDivisibleBy17(limit) {
+    let count = 0;
+    for (let i = 1; i <= limit; i++) {
+        for (let j = i + 1; j <= limit; j++) {
+            if ((i + j) % 17 == 0) {
+                console.log(`(${i},${j}) = ${i + j}`);
+                count++;
+            }
+        }
+    }
+    console.log(`\n Tổng cộng: ${count} cặp`);
+
+}
+// Call hàm findPairDivisibleBy17
+findPairDivisibleBy17(100);
